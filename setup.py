7843ddeeb553c439678198e547b94c0c2422a974
Victor Fontes <vitufc@gmail.com>
2010-10-24 05:20:31 -0200

    Setup.py to bem pip friendly

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+ 
+setup(name='simple_preferences',
+    url='http://github.com/victorfontes/django-simple-preferences',
+    packages=find_packages(),
+    classifiers=[
+        'Framework :: Django',
+    ]
+)
